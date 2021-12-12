b7c5795398cc7eafba16d4288c85b1ad442e2656
OzerOzturk <ozerozturk1636@gmail.com>
2021-12-12 15:05:22 +0300

    tag your friend feature added and completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,75 @@
-import { useEffect, useRef } from 'react';
-import * as faceapi from 'face-api.js'
 import './App.css';
+import Navbar from './components/Navbar';
+import { useEffect, useState } from "react";
+import NewPost from './components/NewPost';
+
 
 function App() {
-const imgRef= useRef();
-const canvasRef= useRef();
 
-const handleImage = async () => {
-  const detections = await faceapi
-    .detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions())
-    .withFaceLandmarks()
-    .withFaceExpressions();
+  const [file, setFile] = useState();
+  const [image, setImage] = useState();
 
-    canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(imgRef.current);
-    faceapi.matchDimensions(canvasRef.current, {
-      width:950,
-      height:750,
-    })
+  useEffect(() => {
+    const getImage = () => {
+      const img = new Image();
+      img.src=URL.createObjectURL(file);
+      img.onload = ()=> {
+        setImage({
+          url:img.src,
+          width:img.width,
+          height:img.height,
+        });
+      };
+    };
 
-    const resized = faceapi.resizeResults(detections,{
-      width:950,
-      height:750,
-    })
-
-    faceapi.draw.drawDetections(canvasRef.current,resized);
-    faceapi.draw.drawFaceExpressions(canvasRef.current,resized);
-    faceapi.draw.drawFaceLandmarks(canvasRef.current,resized);
-};
-
-useEffect(() => {
- const loadModels =() => {
-   Promise.all([
-      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-      faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-   ])
-    .then(handleImage)
-    .catch((e) => console.log(e))
- };
-
- imgRef.current && loadModels();
-  
-},[]);
-
-
+    file && getImage();
+  }, [file]); 
 
   return (
-    <div className="app">
-        <img
-        crossOrigin='anonymous'
-        ref={imgRef}
-        src="https://images.unsplash.com/photo-1590650486895-79681b6f26a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" 
-        alt="" 
-        width='950' h
-        eight='750' />
-        <canvas ref={canvasRef} width='950' height='750'/>
+    <div>
+      <Navbar/>
+      {image ? (<NewPost image={image}/>) : (
+
+      
+      <div className="newPostCard">
+        <div className="addPost">
+          <img
+            src="https://images.unsplash.com/photo-1634127523572-4f7f58d2d710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=762&q=80"
+            alt=""
+            className="avatar"
+          />
+          <div className="postForm">
+            <input type="text" 
+              placeholder="let's put a smile  "
+              className="postInput"
+            />
+            <label htmlFor="file">
+              <img
+                  className="addImg"
+                  src="https://cdn.icon-icons.com/icons2/564/PNG/512/Add_Image_icon-icons.com_54218.png"
+                  alt=""
+                />
+                <img
+                  className="addImg"
+                  src="https://icon-library.com/images/maps-icon-png/maps-icon-png-5.jpg"
+                  alt=""
+                />
+                <img
+                  className="addImg"
+                  src="https://d29fhpw069ctt2.cloudfront.net/icon/image/84451/preview.svg"
+                  alt=""
+                />
+                <button>Send</button>
+            </label>
+            <input 
+            onChange={(e)=> setFile(e.target.files[0])}
+            id="file" style={{display:"none"}} type="file" />
+          </div>
+        </div>
+      </div>
+      )}
     </div>
   );
-};
+}
 
 export default App;
